44b8a7c959c580a455c7eca3c03acc736ed48abd
DavidRodriguezRuiz <davidrodriguezruiz7441@gmail.com>
2024-04-17 18:49:44 +0200

    Actualizacion 2

--- PracticaAlgoritmos/src/app/Principal.java
+++ PracticaAlgoritmos/src/app/Principal.java
@@ -4,12 +4,19 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		
+		int numero;
 		int[] numeros = new int[5];
+		int[] resultadoFactorial = new int[5];
 		
 		for (int i=1; i<numeros.length; i++){
 			numeros[i] = (int) Math.floor(Math.random() * (10 - 1 + 1)) + 1;
 		}
 		
+		
+		for (int i=1; i<=numeros.length; i++) {
+			numero = numeros[i];
+			Factorial(numero);
+		}
 		
 	}
 	
--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -1,18 +1,34 @@
 package util;
 
 public abstract class Algoritmos {
-	
+
 	/**
-     * Devuelve el factorial del numero especificado.
-     * 
-     * @param numero	el numero del que queremos calcular el factorial.
-     * @return			total del factorial del numero especificado.
-     */
-	public static int [] factorial (int[] numeros){
-		int[] factorial = new int [numeros.length];
-		for (int i=1; i<=numeros.length; i++) {
-			factorial[i] *= i;
+	 * Devuelve el factorial del numero especificado.
+	 * 
+	 * @param numero el numero del que queremos calcular el factorial.
+	 * @return total del factorial del numero especificado.
+	 */
+	public static int Factorial(int numero) {
+		int factorial = 1;
+		if (numero == 0 || numero == 1) {
+			return 1;
+		}
+		for (int i = 1; i < numero; i++) {
+			factorial = i * factorial;
 		}
 		return factorial;
+
+	}
+
+	public static int Fibonacci(int numero) {
+		int num1 = 0, num2 = 1, fibonacci = 1;
+		System.out.println(num1);
+		for (int i = 1; i < numero; i++) {
+			System.out.println(fibonacci);
+			fibonacci = num1 + num2;
+			num1 = num2;
+			num2 = fibonacci;
+		}
+		return fibonacci;
 	}
 }
